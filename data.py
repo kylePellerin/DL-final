ab24c22d18f65735262c27e49f2ddd717a74c6c5
Mingi Kang <mkang2@bowdoin.edu>
2026-05-08 20:01:04 -0400

    data information

---
+++ data.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+import os 
+import sys
