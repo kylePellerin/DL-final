9847a96d15ab215c0e61ae6c38ac96e852ee32e8
izzytsuch <itsuchitori@bowdoin.edu>
2026-05-13 20:31:18 -0400

    Resolve merge conflicts by accepting upstream changes

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, send_file, jsonify
+# from shapely import transform
 import torch
 import io
+import numpy as np
 from PIL import Image
 from torchvision.utils import make_grid
 
-from data import generator_DCGAN, noise_2d, countries_mapping, class_mapping, majors_mapping, show_generated_images 
+from data import countries_mapping, class_mapping, majors_mapping
+from models import generator_DCGAN
+from classifier_models import BowdoinClassifier
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -62,6 +67,22 @@ def generate():
     buf.seek(0)
 
     return send_file(buf, mimetype="image/png")
+
+@app.route("/findAttributes", methods=["POST"])
+def findAttributes():
+    data = request.files["image"].read()
+    img = Image.open(io.BytesIO(data)).convert("RGB")
+    img = img.resize((800, 600)) # Resize to match training data
+    img_tensor = torch.from_numpy(np.array(img)).permute(2, 0, 1).float() / 255.0 # (C, H, W) in [0,1]
+    bowdoin_classifier = torch.load("bowdoin_classifier.pth", map_location=device)
+    
+    bowdoin_classifier.eval()
+    class_year, country, major = bowdoin_classifier.forward(img_tensor.unsqueeze(0).to(device)) # Add batch dimension
+    return jsonify({
+        "class year probability distribution": class_year,
+        "possible study abroad countries": country,
+        "possible majors": major
+    })
 
 
 if __name__ == "__main__":
